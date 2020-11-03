a7658a8b8560702ad161aef7b4616b7051e514a3
sepehr-gh <sepehrgh01@gmail.com>
2020-11-03 11:14:06 +0330

    Init

---
+++ src/main/java/lab/idioglossia/gitblog/GitBlogApplication.java
@@ -0,0 +1,13 @@
+package lab.idioglossia.gitblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lab/idioglossia/gitblog/GitBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package lab.idioglossia.gitblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
