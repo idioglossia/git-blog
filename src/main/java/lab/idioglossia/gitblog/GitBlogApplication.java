00d4821dbea60922e22d1d3ac8a1ff480dad9da5
sepehr-gh <sepehrgh01@gmail.com>
2020-11-04 10:14:29 +0330

    Added application restart method

--- src/main/java/lab/idioglossia/gitblog/GitBlogApplication.java
+++ src/main/java/lab/idioglossia/gitblog/GitBlogApplication.java
@@ -1,13 +1,28 @@
 package lab.idioglossia.gitblog;
 
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class GitBlogApplication {
+	private static ConfigurableApplicationContext context;
 
 	public static void main(String[] args) {
-		SpringApplication.run(GitBlogApplication.class, args);
+		context = SpringApplication.run(GitBlogApplication.class, args);
+	}
+
+	public synchronized static void restart() {
+		ApplicationArguments args = context.getBean(ApplicationArguments.class);
+
+		Thread thread = new Thread(() -> {
+			context.close();
+			context = SpringApplication.run(GitBlogApplication.class, args.getSourceArgs());
+		});
+
+		thread.setDaemon(false);
+		thread.start();
 	}
 
 }
